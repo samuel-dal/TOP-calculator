2b1d2936d0bbe5d10661f4feb5372473eee0c0a3
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-04-18 22:56:09 +0800

    refactor: major JS overhaul and removed keydown event listener

--- calculator.js
+++ calculator.js
@@ -1,173 +1,372 @@
-const allBtns = document.querySelectorAll('.bottom-container .btn-container button');
 const resultNum = document.querySelector('.result-num');
-const allOperations = document.querySelectorAll('.operations');
-const screenResult = document.querySelector('.screen-result');
 const root = document.querySelector(':root');
+const screenResult = document.querySelector('.screen-result');
+const allBtns = document.querySelectorAll('.bottom-container .btn-container button');
+const operatorsBtns = document.querySelectorAll('.operators'); 
+const fractionBtns = document.querySelectorAll('.fraction');
+const numBtns = document.querySelectorAll('.numbers');
+const equalBtn = operatorsBtns[0];
+const percentBtn = fractionBtns[0];
+const dotBtn = fractionBtns[1];
 
-let numString = '0';
-let parsedNumString = 0;//used to calculate
-let currentSign = '';
-let resultString = '';//used in result `${resultString}${parsedNumString}`
+let stringEl = '';//for UI 
+let arrEl = [];//for calculation
 
-resultNum.textContent = numString;
+const operatorsRegex = /[*/+\-]/g;
+const operatorsRegex2 = /[*/+]/g;
+const numberRegex = /[0-9]/g;
+const numberWithDotRegex = /[0-9.]/g;
 
-const displayText = (stringToDisplay) => {
-  resultNum.textContent = stringToDisplay;
+let activeDecimal = false;
+let isPercent = false;
+let isEqual = false;
+let activeOperator = false;
+let negativeValue = false;
+let changeOperator = false;
+let equationCount = 0;
+
+const update = () => {
+   if (!stringEl.length) {
+      arrEl.push('0');
+   }
+   stringEl = arrEl.join('');
+   resultNum.textContent = stringEl;
 }
+update();
 
+//---------------------------
 const allClear = () => {
-  numString = '0';
-  parsedNumString = 0;
-  currentSign = '';
-  resultString = '';
+   stringEl = '';
+   arrEl = [];
+   strNum = '';
+   strOpe = '';
+   strElArr = [];
+   activeDecimal = false;
+   isPercent = false;
+   isEqual = false;
+   equationCount = 0;
+   update();
 }
 
 const clearEntry = () => {
-  if (numString.length > 1 && numString !== '0') {
-    numString = numString.substring(0, numString.length -1);
-      return displayText(numString);
-  }
+   const arrElLength = arrEl.length;
 
-   allClear();
-  displayText(numString);
+   if (!arrElLength || arrElLength < 2) {
+      allClear();
+      return;
+   }
+
+   if (negativeValue && arrEl[arrElLength - 2].match(operatorsRegex)) {
+      negativeValue = false;
+      arrEl.pop();
+      activeOperator = true;
+      return update();
+   }
+
+   if (arrEl[arrElLength - 1].includes('.')) {
+      activeDecimal = false;
+      isPercent = false;
+   }
+   isEqual = false;
+   arrEl.pop();
+   update();
 }
 
-const inputBtn = (eventTextContent) => {
-  if (numString.length === 1 && numString === '0') {
-    numString = '';
-  }
-  numString += eventTextContent;
-  displayText(numString);
-}
+const percentage = (isCalculate) => {
 
-
-const pushSign = (event) => {
-  currentSign = event.target.value;//string like times,plus,minus,divide
-  parsedNumString += parseFloat(numString);
-  numString = '';
-  console.log(parsedNumString);
-}
-
-const equalSign = () => {
-  switch (currentSign) {
-    case 'plus':
-      parsedNumString += parseFloat(numString);
+   if (!isCalculate || isPercent || isEqual || activeDecimal) {
+      //console.log('disable percentage');
       return;
+   }
 
-    case 'minus':
-      parsedNumString -= parseFloat(numString);
-      return;
+   if (arrEl[arrEl.length - 1] === '0' && arrEl[0] === '0') {
+       arrEl.push('.', '0', '0');
+       return update();
+   }
 
-    case 'times':
-      parsedNumString *= parseFloat(numString);
-      return;
+   let percentArr = [];
+   let stop = false;
+   for (let i = arrEl.length - 1; i >= 0; i--) {
+      const lastVal = arrEl[i];
 
-    case 'divide':
-      parsedNumString /= parseFloat(numString);
-      return;
-
-  }
-}
-
-let resultSize = 4;
-
-let changeStylesArr = [['--number-result-justify', '--number-result-align', '--number-result-word-break', '--number-result-overflow'], // changeStylesArr[0] :root variables
-                       ['start', 'start', 'break-all', 'auto'],//changeStylesArr[1] toggle css style
-                       ['end', 'center', 'none', 'none']];//changeStylesArr[2] default css style;
-
-const resultDisplayBehavior = () => {
-  if (screenResult.clientWidth <  resultNum.clientWidth) {
-    resultSize = 2;
-    root.style.setProperty('--result-number-size', `${resultSize}rem`);
-
-    if (resultSize === 2) {
-      for (let i = 0; i < changeStylesArr[0].length; i++){
-        root.style.setProperty(changeStylesArr[0][i], changeStylesArr[1][i]);
+      if (lastVal.match(operatorsRegex)) {
+         stop = true;
       }
-      return;
-    }
 
-    return;
-  } 
+      if (!stop) {
+         percentArr.unshift(lastVal);
+         arrEl.pop();
+      }
+   }
 
-  if (screenResult.clientWidth >  resultNum.clientWidth && numString.length <= 10) {//bug
-    resultSize = 4;
+   let strPercentage = String((parseFloat(percentArr.join('')) / 100).toFixed(2));
 
-    for (let i = 0; i < changeStylesArr[0].length; i++){
-      root.style.setProperty(changeStylesArr[0][i], changeStylesArr[2][i]);
-    }
-    root.style.setProperty('--result-number-size', `${resultSize}rem`);
-  }
+   if (parseFloat(strPercentage)) {
+      for (let i = 0; i < strPercentage.length; i++) {
+         arrEl.push(strPercentage[i]);
+      }
+   }
+   isPercent = true;
+   update();
 }
 
+ let resultSize = 4;
 
-allBtns.forEach(btn => {
-  btn.addEventListener('mousedown', (mousedownEvent) => {
-    let targetValue = mousedownEvent.target.value;
-    let classValue = mousedownEvent.target.classList.value;
-    let contentValue = mousedownEvent.target.textContent;
+ let changeStylesArr = [
+   ['--number-result-justify', '--number-result-align', '--number-result-word-break', '--number-result-overflow'], 
+   ['start', 'start', 'break-all', 'auto'], ['end', 'center', 'none', 'none']
+ ];
 
-    if (classValue === 'numbers' || classValue === 'operations') {
-       resultString += contentValue;
+ const resultDisplayBehavior = () => {
+   if (screenResult.clientWidth <  resultNum.clientWidth) {
+     resultSize = 2;
+     root.style.setProperty('--result-number-size', `${resultSize}rem`);
+
+     if (resultSize === 2) {
+       for (let i = 0; i < changeStylesArr[0].length; i++){
+         root.style.setProperty(changeStylesArr[0][i], changeStylesArr[1][i]);
+       }
+       return;
      }
 
-    if (targetValue === 'all-clear') {
-       allClear();
-       displayText(numString);
-       return;
-    }
+     return;
+   } 
 
-    if (targetValue === 'clear-entry') {
-      return clearEntry();
-    }
+   if (screenResult.clientWidth >  resultNum.clientWidth && stringEl.length <= 10) {//bug
+     resultSize = 4;
 
-    if (classValue === 'numbers') {
-      inputBtn(contentValue);
-      displayText(numString);
-      return;
-    }
+     for (let i = 0; i < changeStylesArr[0].length; i++){
+       root.style.setProperty(changeStylesArr[0][i], changeStylesArr[2][i]);
+     }
+     root.style.setProperty('--result-number-size', `${resultSize}rem`);
+   }
+ }
 
-    if (classValue === 'operations' && targetValue !== 'equal') {
-      if (currentSign && targetValue === 'minus' || !currentSign & numString === '0') {
-        inputBtn(contentValue);
-        return;
+//-------------------------------
+
+const separateTypes = () => {
+   let strNum = '';
+   let strOpe = '';
+   let strElArr = [];
+   let stringElLength = stringEl.length;
+
+   for (let i = 0; i < stringElLength; i++) {
+      let currentVal = stringEl[i];
+
+      if (strOpe.length > 1 && currentVal.match(numberWithDotRegex)) {
+         strNum += strOpe[1];
       }
-      pushSign(mousedownEvent);
-      displayText(numString);
+
+      if (currentVal.match(numberWithDotRegex)) {
+          strNum += currentVal;
+      }
+
+      if (currentVal.match(operatorsRegex)) {
+          strOpe += currentVal;
+      }
+
+      if (strNum && currentVal.match(operatorsRegex) || i === stringElLength - 1 && strNum !== '') {
+         strElArr.push(strNum);
+         strNum = '';
+      }
+
+      if (strOpe && currentVal.match(numberWithDotRegex) || i === stringElLength - 1 && strOpe !== '') {
+         strElArr.push(strOpe[0]);
+         strOpe = '';
+      }
+         
+   }
+   resultNum.textContent = stringEl;
+   return strElArr;
+}
+
+const calculate = (convertedArray) => {//left to right
+   let firstCalculation = true;
+   let calculation = 0;
+   let modifiedArr = [];
+
+   //firstCalculation
+   for (let i = 0; i < convertedArray.length; i++) {
+      modifiedArr.push(convertedArray[i + 3]);
+
+      if (firstCalculation) {
+         const firstVal = parseFloat(convertedArray[i]);
+         const secondVal = convertedArray[i + 1];//operator
+         const thirdVal = parseFloat(convertedArray[i + 2]);
+
+         if (secondVal === '+') {
+            calculation += firstVal + thirdVal;
+         }
+
+         if (secondVal === '-') {
+            calculation += firstVal - thirdVal;
+         }
+
+         if (secondVal === '*') {
+            calculation += firstVal * thirdVal;
+         }
+
+         if (secondVal === '/') {
+            calculation += firstVal / thirdVal;
+         }
+
+         firstCalculation = false;
+      }
+   }
+   //calculate all
+   for (let i = 0; i < modifiedArr.length; i++) {
+      const firstVal = modifiedArr[i];
+      const secondVal = parseFloat(modifiedArr[i + 1]);
+
+      if (firstVal === '+') {
+         calculation += secondVal;
+      }
+
+      if (firstVal === '-') {
+         calculation -= secondVal;
+      }
+         
+      if (firstVal === '*') {
+         calculation *= secondVal;
+      }
+
+      if (firstVal === '/') {
+         calculation /= secondVal;
+      }
+   }
+   return calculation;
+}
+
+numBtns.forEach(numItem => {
+   numItem.addEventListener('mousedown', (numItemEvent) => {
+      const eventTarget = numItemEvent.target;
+      const targetValue = eventTarget.value;
+      const arrElLength = arrEl.length;
+
+      if (isEqual && targetValue === '0') {
+         allClear();
+         return update();
+      }
+
+      if (isEqual) {
+         allClear();
+      }
+
+      if (arrEl[0] === '0' && arrElLength < 2) {
+         arrEl.pop();
+      }
+
+      if (equationCount < 1 || equationCount >= 2) {
+         equationCount++;
+      }
+
+      negativeValue = false;
+      arrEl.push(targetValue);
+      //percentage(activeDecimal);
+      update();
+   });
+});
+
+
+operatorsBtns.forEach(opeItem => {
+   opeItem.addEventListener('mousedown', (opeItemEvent) => {
+      const eventTarget = opeItemEvent.target;
+      const targetValue = eventTarget.value;
+      const arrElLength = arrEl.length
+      const lastIndex = arrEl[arrElLength - 1];
+      const secondLastIndex = arrEl[arrElLength - 2];
+
+      if (negativeValue || lastIndex.match(operatorsRegex) && targetValue === '-') {
+         if (negativeValue) {
+            arrEl.pop();
+         }
+
+         if (lastIndex.match(operatorsRegex2) && targetValue === '-') {
+            arrEl.push(targetValue);
+            negativeValue = true;
+            update();
+            return;
+         }
+
+         arrEl.pop();
+         arrEl.push(targetValue, '-');
+         activeOperator = true;
+         negativeValue = true;
+         update();
+         return;
+      }
+
+      if (negativeValue && secondLastIndex.match(operatorsRegex) || 
+         isEqual || 
+         equationCount < 3 && targetValue === '=') {
+            return;
+      }
+
+      if (lastIndex.match(operatorsRegex) && secondLastIndex.match(numberWithDotRegex)) {
+         console.log('operator');
+         arrEl.pop();
+         arrEl.push(targetValue);
+         update();
+         activeOperator = true;
+         return;
+      }
+
+      if (equationCount < 2) {
+         equationCount++;
+      }
+
+      if (arrEl[0] === '0' && arrElLength < 2) {
+         equationCount++;
+      }
+
+      arrEl.push(targetValue);
+      update();
+      isPercent = false;
+      activeDecimal = false;
+
+      if (targetValue === '=' && equationCount > 2) {
+         if (lastIndex.match('-') && secondLastIndex.match(operatorsRegex) || isEqual) {
+            return;
+         }
+         resultNum.textContent += calculate(separateTypes());
+         isEqual = true;
+      }
+   });
+});
+
+percentBtn.addEventListener('mousedown', (event) => {
+   if (activeDecimal || isEqual) {
       return;
-    }
+   }
 
-    if (targetValue === 'equal') {
-      equalSign();
-      displayText(`${resultString}${parsedNumString}`);
-      allClear();
-      return
-    }
+   if (equationCount < 1) {
+      equationCount++;
+   }
 
-    // numString += contentValue;
-    // displayText(numString);
-  })
+   if (!arrEl.length || !stringEl[stringEl.length - 1].match(numberRegex)) {
+      percentage(false);
+      activeDecimal = false;
+      isPercent = false;
+      return;
+   }
+
+   if (!activeDecimal) {
+      percentage(true);
+      activeDecimal = true;
+      isPercent = true;
+      return;
+   }
 });
 
-document.addEventListener('keydown', (keydownEvent) => {
-  if (parseInt(keydownEvent.key) || keydownEvent.key === '0'){
-    inputBtn(keydownEvent.key);
-    resultString += keydownEvent.key;
-  }
-
-  if (keydownEvent.key === 'Backspace') {
-    clearEntry();
-  }
-
-  if (keydownEvent.key === 'Enter') {
-    equalSign();
-    resultString += '=';
-    displayText(`${resultString}${parsedNumString}`);
-    allClear();
-  }
-  resultDisplayBehavior();
+dotBtn.addEventListener('mousedown', (event) => {
+   if (activeDecimal || isEqual) {
+      return;
+   }
+   activeDecimal = true;
+   arrEl.push('.');
+   update();
 });
 
-document.addEventListener('mousedown', (e) => {
-  resultDisplayBehavior();
+document.addEventListener('mousedown', () => {
+   resultDisplayBehavior();
 });
